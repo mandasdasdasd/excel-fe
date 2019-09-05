602bab79793fe0575a13cd2c02c73584020cf827
Your Name <you@example.com>
2019-09-05 10:19:53 +0800

    提一下

--- ff.py
+++ ff.py
@@ -5,40 +5,106 @@ import pymysql
 import json
  
 db = pymysql.connect("159.226.193.219","mysql","mysql","ysman" )
-cursor = db.cursor()
-#cursor.execute("SELECT VERSION()")
+cursor =db. cursor()
 #data = cursor.fetchone()
-
 
 app = Flask(__name__)
 api = Api(app)
+
+class YearSort(Resource):
+    def __init__(self):
+        self.get_args = reqparse.RequestParser()
+        self.get_args.add_argument("year",  type=int)
+        self.args = self.get_args.parse_args()
+
+    def get(self):
+        year = self.args["year"]
+        db = pymysql.connect("159.226.193.219","mysql","mysql","ysman" )
+        acursor = db.cursor()
+        sql = '''select * from hh where year=%d''' % year
+        acursor.execute(sql)
+        res = acursor.fetchall()
+        ll = []
+        for one in res:
+            dd = {}
+            dd["id"] = one[0]
+            dd["vname"] = one[1]
+            dd["input"] = one[2]
+            dd["output"] = one[3]
+            dd["discount"] = one[4]
+            dd["profit"] = (one[3] - one[2]*one[4]) * one[5]
+            dd["number"] = one[5]
+            dd["status"] = one[6]
+            dd["year"] = one[7]
+            dd["people"] = one[8]
+            ll.append(dd)
+        return ll
+
+class Year(Resource):
+    def get(self):
+        db = pymysql.connect("159.226.193.219","mysql","mysql","ysman" )
+        acursor = db.cursor()
+        sql = '''select years from xyear'''
+        acursor.execute(sql)
+        res = acursor.fetchall()
+        ll = []
+        for one in res:
+            ll.append(one[0])
+        return ll
 
 class Save(Resource):
     def __init__(self):
         self.get_args = reqparse.RequestParser()
         self.get_args.add_argument("data",  type=str)
+        self.get_args.add_argument("year",  type=int)
         self.args = self.get_args.parse_args()
 
     def get(self):
+        cursor = db.cursor()
         data = self.args["data"]
+        year = self.args["year"]
         for one in json.loads(data):
-            print(one["sex"])
-            #sql = '''insert into table_name (sex, name, age) values (%d, %d, %d)''' % ()
+            sql = '''insert into hh (year, name, input, output, discount, number, status, people) values (%d, '%s', %d, %d, %f, %d,  '%s', '%s')''' % (int(one["year"]), one["vname"], int(one["input"]), int(one["output"]), float(one["discount"]), int(one["number"]), one["status"], one['people'])
+            cursor.execute(sql)
+            db.commit()
+        obj = HelloWorld()
+        data = obj.get(year)
+        return data
 
 class HelloWorld(Resource):
-    def get(self):
-        return [
-                {'sex': 1, 'age': 2, 'name': 3},
-                {'sex': 1, 'age': 2, 'name': 3},
-                {'sex': 1, 'age': 2, 'name': 3},
-                {'sex': 1, 'age': 2, 'name': 3},
-                {'sex': 1, 'age': 2, 'name': 3},
-                {'sex': 1, 'age': 2, 'name': 3},
-                ]
+    def __init__(self):
+        self.get_args = reqparse.RequestParser()
+        self.get_args.add_argument("year",  type=int)
+        self.args = self.get_args.parse_args()
+
+    def get(self, year=2019):
+        year = self.args["year"]
+        print(year)
+
+        sql = '''select * from hh where year=%d''' % year
+        cursor.execute(sql)
+        res = cursor.fetchall()
+        ll = []
+        for one in res:
+            dd = {}
+            dd["id"] = one[0]
+            dd["vname"] = one[1]
+            dd["input"] = one[2]
+            dd["output"] = one[3]
+            dd["discount"] = one[4]
+            dd["profit"] = (one[3] - one[2]*one[4]) * one[5]
+            dd["number"] = one[5]
+            dd["status"] = one[6]
+            dd["year"] = one[7]
+            dd["people"] = one[8]
+            ll.append(dd)
+        return ll
 
 
 api.add_resource(HelloWorld, '/init')
 api.add_resource(Save, '/init/add')
+api.add_resource(Year, '/init/year')
+api.add_resource(YearSort, '/init/yearsort')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,59 +2,116 @@
     <div>
         <vxe-toolbar>
           <template v-slot:buttons>
-            <!-- 
-            <vxe-button @click="insertEvent()">在第1行插入</vxe-button>
-            <vxe-button @click="insertEvent(tableData[2])">在第3行插入并激活 Sex 单元格</vxe-button>
-            -->
-            <vxe-button @click="getInsertEvent">获取新增</vxe-button>
-            <vxe-button @click="insertEvent(-1)">在最后行插入</vxe-button>
+            <vxe-button @click="getInsertEvent">保存</vxe-button>
+            <vxe-button @click="insertEvent(-1)">新增</vxe-button>
+             <vxe-button>
+              <template>{{year}}</template>
+              <template v-slot:dropdowns>
+                <vxe-button @click="xyear(x)" v-for="x in years">{{x}}</vxe-button>
+              </template>
+            </vxe-button>
           </template>
         </vxe-toolbar>
-
-        <vxe-table
+        
+     <vxe-table
           border
-          show-overflow
           ref="xTable"
-          max-height="400"
+          class="mytable-style"
+          :header-cell-class-name="headerCellClassName"
+          :cell-class-name="cellClassName"
+          :row-class-name="rowClassName"
           :data="tableData"
           :edit-config="{trigger: 'click', mode: 'cell'}">
           <vxe-table-column type="index" width="60"></vxe-table-column>
-          <vxe-table-column field="sex" title="Sex" :edit-render="{name: 'input'}"></vxe-table-column>
-          <vxe-table-column field="name" title="Name" :edit-render="{name: 'input'}"> </vxe-table-column>
-          <vxe-table-column field="age" title="Age" :edit-render="{name: 'input'}"> </vxe-table-column>
+          <vxe-table-column field="year" title="年份"></vxe-table-column>
+          <vxe-table-column field="vname" title="项目名称" sortable  :edit-render="{name: 'input'}"></vxe-table-column>
+          <vxe-table-column field="people" title="人员" sortable  :edit-render="{name: 'input'}"></vxe-table-column>
+          <vxe-table-column field="input" title="进价"  :edit-render="{name: 'input'}"></vxe-table-column>
+          <vxe-table-column field="output" title="卖价" :edit-render="{name: 'input'}"></vxe-table-column>
+          <vxe-table-column field="discount" title="折扣" :edit-render="{name: 'input'}"></vxe-table-column>
+          <vxe-table-column field="profit" title="利润"></vxe-table-column>
+          <vxe-table-column field="number" title="数量" :edit-render="{name: 'input'}"></vxe-table-column>
+          <vxe-table-column field="status" title="状态" sortable :edit-render="{name: 'input'}"></vxe-table-column>
         </vxe-table>
+
         </div>
 </template>
+
+
+
 <script>
+import store from "./store.js"
+
      export default {
           data () {
             return {
               tableData: [],
-              asdasd: []
+              year: sessionStorage.getItem('year'),
+              years: []
             }
           },
 
         mounted: function () {   //页面初始化方法
-            this.$http.get('/init').then(response => {
+           this.$http.get('/init', {params: {year: this.year}}).then(response => {
                 this.tableData = response.data
+            }),
+            this.$http.get('/init/year').then(response => {
+                this.years = response.data;
             })
+
         },
 
           methods: {
+            xyear (item) {
+                this.year = item
+                sessionStorage.setItem('year', item)
+                this.$http.get('/init/yearsort', {params: {year: item}}).then(response => {
+                    this.tableData = response.data
+                })
+            },
+
             insertEvent (row) {
               let record = {
-              sex : "1"
+                year: this.year
               }
               this.$refs.xTable.insertAt(record, row)
-                .then(({ row }) => this.$refs.xTable.setActiveCell(row, 'sex'))
+                .then(({ row }) => this.$refs.xTable.setActiveCell(row, 'year'))
             },
+
+            rowClassName ({ row, rowIndex}) {
+                if (row.status === "完成") {
+                    return 'row-green'
+                    }
+                else if (row.status === "紧急") {
+                    return 'row-red'
+                    }
+                else if (row.status === "失效") {
+                    return 'row-gray'
+                    }
+            },
+
             getInsertEvent () {
+               alert()
               let insertRecords = this.$refs.xTable.getInsertRecords()
-                 //this.$http.get('/init/add', {params: {data: JSON.stringify(insertRecords[0])}}).then(response => {
-                 this.$http.get('/init/add', {params: {data: JSON.stringify(insertRecords)}}).then(response => {
-                    alert("success")
+                 this.$http.get('/init/add', {params: {data: JSON.stringify(insertRecords), year: this.year}}).then(response => {
+                    this.tableData = response.data
                 })
             }
+
           }
         }
 </script>
+<style >
+.vxe-table .vxe-body--row.row-green {
+  background-color: green;
+  color: #fff;
+}
+.vxe-table .vxe-body--row.row-red {
+  background-color: red;
+  color: #fff;
+}
+.vxe-table .vxe-body--row.row-gray {
+  background-color: gray;
+  color: #fff;
+}
+</style>
